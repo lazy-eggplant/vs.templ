c9c93b10b3e8952f461e30753f24e9c8790448e9
checkroom <checkroom@lenovos-novos.lan>
2024-11-02 13:59:05 +0000

    Updating CLI and adding input stream mode

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,12 +1,12 @@
 /*
     CLI Usage:
-    vs.tmpl TEMPLATE DATA [NAMESPACE='s']
+    vs.tmpl <template-file> <data-file> [namespace=`s:`]
 
     Unlike its usage in vs.fltk, template must be specified on its own.
 
     Alternative
 
-    vs.tmpl [NAMESPACE='s']
+    vs.tmpl [namespace=`s:`]
     
     with both files added via pipes, like `vs.tmpl <(cat template.xml) <(cat data.xml)
 */
@@ -18,29 +18,54 @@
 
 using namespace vs::templ;
 
+//TODO: Support error logging on std::cerr. Maybe use VS_VERBOSE env variable to determine what is shown and if.
 int main(int argc, const char* argv[]){
     const char* ns_prefix="s:";
-    if(argc<3){
-        std::cerr<<"Wrong usage:\n\t"<<argv[0]<<" <template-file> <data-file> [prefix=`s:`]\nOR\t "<<argv[0]<<"[prefix=`s:`] and two input streams\n";
+    if(argc==0 || argc > 3){
+        std::cerr<<"Wrong usage:\n\t"<<argv[0]<<" <template-file> <data-file> [namespace=`s:`]\nOR\t "<<argv[0]<<"[namespace=`s:`] and two input streams\n";
         exit(1);
     }
 
     pugi::xml_document data, tmpl;
-    data.load_file(argv[2]);
-    tmpl.load_file(argv[1]);
 
-    if(argc==4){ns_prefix=argv[3];}
-    
-    preprocessor doc(data,tmpl,ns_prefix);
-    doc.parse().save(std::cout);
+    if(argc>=2){
+        {auto t = tmpl.load_file(argv[1]); if(!t){std::cerr<<t.description();exit(2);}}
+        {auto t = data.load_file(argv[2]); if(!t){std::cerr<<t.description();exit(3);}}
 
-    /*
-    for(auto& log : doc.logs){
-        if(log.type==log_t::types::ERROR){
-            std::cerr<<log;
+        if(argc==4){ns_prefix=argv[3];}
+        
+        preprocessor doc(data,tmpl,ns_prefix);
+        auto& result = doc.parse();
+
+        /*
+        for(auto& log : doc.logs){
+            if(log.type==log_t::types::ERROR){
+                std::cerr<<log;
+            }
         }
+        */
+
+        result.save(std::cout);
     }
-    */
-    
+    else{
+        if(argc==2){ns_prefix=argv[1];}
+
+        {auto t = tmpl.load(std::cin); if(!t){std::cerr<<t.description();exit(2);}}
+        {auto t = data.load(std::cin); if(!t){std::cerr<<t.description();exit(3);}}
+
+        preprocessor doc(data,tmpl,ns_prefix);
+        auto& result = doc.parse();
+
+        /*
+        for(auto& log : doc.logs){
+            if(log.type==log_t::types::ERROR){
+                std::cerr<<log;
+            }
+        }
+        */
+
+        result.save(std::cout);
+    }
+
     return 0;
 }
