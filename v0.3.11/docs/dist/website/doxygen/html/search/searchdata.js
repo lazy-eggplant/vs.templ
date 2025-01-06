b095297fd3f2127529a5de507089be4b2b0c84b2
KaruroChori <KaruroChori@users.noreply.github.com>
2025-01-06 08:58:21 +0000

    deploy: 4585c8568a6521c02f8292d3a53fdede531ab069

---
+++ v0.3.11/docs/dist/website/doxygen/html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "ceglprstv",
+  1: "cglprst",
+  2: "v",
+  3: "el",
+  4: "l"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "typedefs"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Typedefs"
+};
+
