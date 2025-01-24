fa85a7c9689fa6e834cc451b2ce95cdfa49acb9c
KaruroChori <KaruroChori@users.noreply.github.com>
2025-01-24 11:52:27 +0000

    deploy: 59c2035c9958ff75b069a7b2decadb53e1361a08

--- next/doxygen/search/classes_6.js
+++ next/doxygen/search/classes_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['symbol_5fmap_0',['symbol_map',['../structvs_1_1templ_1_1symbol__map.html',1,'vs::templ']]]
+  ['symbol_0',['symbol',['../structvs_1_1templ_1_1symbol.html',1,'vs::templ']]],
+  ['symbol_5fmap_1',['symbol_map',['../structvs_1_1templ_1_1symbol__map.html',1,'vs::templ']]]
 ];
