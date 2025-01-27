cc6b5b6e6ed0922371c9824ba17313aac450c4e4
KaruroChori <KaruroChori@users.noreply.github.com>
2025-01-27 11:46:22 +0000

    deploy: a8206ae178f3cbd18af7ee40fe2e71826e458b21

--- next/doxygen/search/classes_0.js
+++ next/doxygen/search/classes_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['config_5ft_0',['config_t',['../structvs_1_1templ_1_1preprocessor_1_1config__t.html',1,'vs::templ::preprocessor']]],
-  ['ctx_5flog_1',['ctx_log',['../structvs_1_1templ_1_1ctx__log.html',1,'vs::templ']]]
+  ['ctx_1',['ctx',['../structvs_1_1templ_1_1log__t_1_1ctx.html',1,'vs::templ::log_t']]]
 ];
