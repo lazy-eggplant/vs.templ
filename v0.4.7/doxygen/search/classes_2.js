b1a812dd73f133bd6e2b9a4685c1dc5b441a7dc6
KaruroChori <KaruroChori@users.noreply.github.com>
2025-04-17 00:32:35 +0000

    deploy: 4d0c54d7874088cfb86732f7071cf45e5fcfd33e

---
+++ v0.4.7/doxygen/search/classes_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['log_5ft_0',['log_t',['../structvs_1_1templ_1_1log__t.html',1,'vs::templ']]]
+];
---
+++ v0.4.7/doxygen/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['guard_0',['guard',['../structvs_1_1templ_1_1symbol__map.html#ad3b0b485610d61fed533be570ca5558c',1,'vs::templ::symbol_map']]]
+];
